277bde5bf01bfd03898f71354a2b8ef5428d4512
unknown <zaryabzak22@gmail.com>
2022-12-30 16:53:15 +0500

    Adding an AuthContext

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import {
   BrowserRouter as Router,
   Redirect,
@@ -10,34 +10,48 @@ import UpdatePlace from "./places/pages/UpdatePlace";
 import UserPlaces from "./places/pages/UserPlaces";
 import MainHeader from "./shared/compnents/Navigation/MainHeader";
 import MainNavigation from "./shared/compnents/Navigation/MainNavigation";
+import { AuthContext } from "./shared/context/auth-context";
 import Auth from "./users/pages/Auth";
 import Users from "./users/pages/Users";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
   return (
-    <Router>
-      <MainNavigation />
-      <main>
-        <Switch>
-          <Route path={"/"} exact>
-            <Users />
-          </Route>
-          <Route path={"/places/new"}>
-            <NewPlace />
-          </Route>
-          <Route path={"/:uid/places"} exact>
-            <UserPlaces />
-          </Route>
-          <Route path={"/places/:pid"}>
-            <UpdatePlace />
-          </Route>
-          <Route path={"/auth"}>
-            <Auth />
-          </Route>
-          <Redirect to={"/"} />
-        </Switch>
-      </main>
-    </Router>
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      <Router>
+        <MainNavigation />
+        <main>
+          <Switch>
+            <Route path={"/"} exact>
+              <Users />
+            </Route>
+            <Route path={"/places/new"}>
+              <NewPlace />
+            </Route>
+            <Route path={"/:uid/places"} exact>
+              <UserPlaces />
+            </Route>
+            <Route path={"/places/:pid"}>
+              <UpdatePlace />
+            </Route>
+            <Route path={"/auth"}>
+              <Auth />
+            </Route>
+            <Redirect to={"/"} />
+          </Switch>
+        </main>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
